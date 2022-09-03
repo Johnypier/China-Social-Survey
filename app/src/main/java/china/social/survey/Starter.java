bc1c996c87d44fe808ef590b965ad00d12aa18f4
Johnpier <nicropro@gmail.com>
2022-09-03 22:18:40 +0200

    init

---
+++ app/src/main/java/china/social/survey/Starter.java
@@ -0,0 +1,9 @@
+package china.social.survey;
+
+import javafx.application.Application;
+
+public class Starter {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
